d469c41e6bf3d777ca87418a18653d68d2bf17b2
wizfury <Aakarsh135@gmail.com>
2023-05-17 11:13:27 +0530

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  return runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp();
+  }
+}
